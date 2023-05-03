9ae660e53d45ac30423958c4a2e324cbaa03c0ae
Lukáš Kromholz <l.kromholz@outlook.com>
2023-05-03 17:35:32 +0200

    New pages

---
+++ src/pages/contacts.tsx
@@ -0,0 +1,9 @@
+const Contacts: React.FC = () => {
+    return (
+      <div>
+        <h1>Contacts Me</h1>
+      </div>
+    );
+  }
+  
+  export default Contacts;
---
+++ src/pages/my-work.tsx
@@ -0,0 +1,9 @@
+const MyWork: React.FC = () => {
+    return (
+      <div>
+        <h1>My-Work</h1>
+      </div>
+    );
+  }
+  
+  export default MyWork;
