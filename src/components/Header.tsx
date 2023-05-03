f9ee10536f2176d33f690f929f10ff9620062f6a
Lukáš Kromholz <l.kromholz@outlook.com>
2023-05-03 20:14:35 +0200

    upd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-100 py-4 shadow-md">
+    <header className="bg-gray-800 py-4 shadow-md">
       <div className="container mx-auto px-4 flex items-center justify-between">
         <Link href="/">
           <a className="flex items-center">
             <Image src="/path/to/your/logo.png" alt="Logo" width={100} height={40} />
-            <span className="text-2xl font-semibold ml-2 text-gray-800">Your Site Name</span>
+            <span className="text-2xl font-semibold ml-2 text-gray-100">Your Site Name</span>
           </a>
         </Link>
         <nav className="flex items-center space-x-4">
@@ -27,7 +27,7 @@ const Header: React.FC = () => {
             <a className="text-lg text-gray-800">Contacts</a>
           </Link>
           <Link href="/login">
-            <a className="bg-gray-800 hover:bg-gray-700 text-white text-lg px-4 py-2 rounded transition">Login</a>
+            <a className="bg-gray-100 hover:bg-gray-200 text-white text-lg px-4 py-2 rounded transition">Login</a>
           </Link>
         </nav>
       </div>
