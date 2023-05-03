15bca859a1e0e5c3867c33c4eb9ddecee556cc63
Lukáš Kromholz <l.kromholz@outlook.com>
2023-05-03 19:10:13 +0200

    upd

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,3 +1,4 @@
+// src/pages/pricing.tsx
 import React from 'react';
 import Header from '../components/Header';
 
@@ -9,7 +10,7 @@ const Pricing: React.FC = () => {
         <h1 className="text-center text-4xl mb-8">Pricing</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {/* Standard Package */}
-          <div className="border p-4 rounded">
+          <div className="bg-gray-100 border p-4 rounded shadow-md hover:shadow-lg transition-shadow duration-300">
             <h2 className="text-2xl mb-4">Standard Package (99€)</h2>
             <ul className="list-disc pl-5">
               <li>3 professionally edited photos</li>
@@ -17,7 +18,7 @@ const Pricing: React.FC = () => {
           </div>
 
           {/* Premium Package */}
-          <div className="border p-4 rounded">
+          <div className="bg-gray-100 border p-4 rounded shadow-md hover:shadow-lg transition-shadow duration-300">
             <h2 className="text-2xl mb-4">Premium Package (349€)</h2>
             <ul className="list-disc pl-5">
               <li>5 beautifully edited photos</li>
@@ -31,7 +32,7 @@ const Pricing: React.FC = () => {
           </div>
 
           {/* Deluxe Package */}
-          <div className="border p-4 rounded">
+          <div className="bg-gray-100 border p-4 rounded shadow-md hover:shadow-lg transition-shadow duration-300">
             <h2 className="text-2xl mb-4">Deluxe Package (649€)</h2>
             <ul className="list-disc pl-5">
               <li>8 professionally edited photos</li>
@@ -47,7 +48,7 @@ const Pricing: React.FC = () => {
         </div>
 
         {/* Tailored Experience */}
-        <div className="border p-4 mt-8 rounded">
+        <div className="bg-gray-100 border p-4 mt-8 rounded shadow-md hover:shadow-lg transition-shadow duration-300">
           <h2 className="text-2xl mb-4">Tailored Experience (1000€)</h2>
           <ul className="list-disc pl-5">
             <li>16 professionally edited photos</li>
